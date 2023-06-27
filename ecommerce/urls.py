4144ebf817b5f5a8a9696d31bf90a857be577ae1
Suraj Kumar <56630693+surajkumar4aug@users.noreply.github.com>
2023-06-27 12:17:58 +0530

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,33 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include,re_path
-from rest_framework_swagger.views import get_swagger_view
-schema_view=get_swagger_view(title='API Documentation')
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Jaseci API",
-        default_version='v1',
-        description="Welcome to the world of Jaseci",
-        terms_of_service="https://www.jaseci.org",
-        contact=openapi.Contact(email=""),
-        license=openapi.License(name=""),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+
 urlpatterns = [
-    re_path(r'^doc(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),  #<-- Here
-    path('doc/', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'),  #<-- Here
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
-         name='schema-redoc'),  #<-- Here
     path('admin/', admin.site.urls),
-    path('',include('blog.urls')),
-    path('',include('customer.urls')),
     path('',include('biodata.urls')),
    # path('',include('Stockinventory.urls')),
     
