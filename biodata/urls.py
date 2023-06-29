f7bae9a231a9a3bf4fbbd325e2849ce1df1d5176
Suraj Kumar <56630693+surajkumar4aug@users.noreply.github.com>
2023-06-29 09:49:21 +0530

    Update urls.py

--- biodata/urls.py
+++ biodata/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import MarriageBiodataViewSet, html_file_view, MarriageBiodataAPIView
+from .views import MarriageBiodataViewSet, html_file_view, MarriageBiodataAPIView,home
 
 router = routers.DefaultRouter()
 router.register(r'marriage-biodata', MarriageBiodataViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('html-file/', html_file_view, name='html_file'),
+    #path('', home, name='html_file'),
+    path('', html_file_view, name='html_file'),
     path('marriage/', MarriageBiodataAPIView.as_view(), name='html_file'),
 ]
