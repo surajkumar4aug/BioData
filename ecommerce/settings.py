f3485a9fb7956d86b0281bcbe65787e78dd3a9cf
Suraj Kumar <56630693+surajkumar4aug@users.noreply.github.com>
2023-06-30 12:33:24 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -77,13 +77,17 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://biodata_user:4QprnkA4M0gRBU4xy3G4uIZdLWYiz9U4@dpg-cif66dh5rnujc4pebbv0-a.oregon-postgres.render.com/biodata'
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
